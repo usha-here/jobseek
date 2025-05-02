4eead2533b63fcfda650b79d4bdaba4ffa28f4e1
usha-here <ushanitwal09@gmail.com>
2025-05-02 16:19:41 +0530

    Refactor Navbar component to include logo and login buttons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { useClerk,UserButton,useUser } from '@clerk/clerk-react'
+
 
 const Navbar = () => {
+
+  const { openSignIn } =useClerk();
+
+
+
+
   return (
-    <div>Navbar</div>
+    <div className='shadow py-4'>
+      <div className='container px-4 2xl:px-20 mx-auto flex  justify-between items center'>
+      <img src={assets.logo} alt=""/>
+      <div className='flex gap-4 max-sm:text-xs'>  
+        <button className='text-gray-600'>Recruiter Login </button>
+        <button  onClick={e=> openSignIn()} className='bg-blue-600 text-white px-8 sm:px-9 py-2 rounded-full'>Login </button>
+      </div>
+    </div>
+    </div>
   )
 }
 
