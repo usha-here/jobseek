f452a66e21cc55417e059ae0c706c088684cc380
usha-here <ushanitwal09@gmail.com>
2025-05-02 16:17:55 +0530

    Add new assets and create ApplyJob page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Route ,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import ApplyJob from './pages/ApplyJob'
+import Applications from './pages/Applications'
 
 const App = () => {
   return (
-    <div>this is a new app for job portal</div>
+    <div>
+      <Routes>
+        <Route path='/' element= {<Home />} />
+        <Route path='/apply-job/:id' element= {<ApplyJob/>} />
+        <Route path='/applications' element= {<Applications/>} />
+      </Routes>
+    </div>
   )
 }
 
